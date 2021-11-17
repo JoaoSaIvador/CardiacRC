7645fc9139f6d092597e71550b5af08537824157
João Salvador <jpvsalvador05@gmail.com>
2021-11-17 12:41:41 +0000

    Patient Dashboard Design

--- CardiacRC-Client/pages/patients/_username/details.vue
+++ CardiacRC-Client/pages/patients/_username/details.vue
@@ -1,51 +1,75 @@
 <template>
     <div>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         <Topbar/>
         <b-container class="page-body">
-            <div >
-                <h2 class="mb-3">Dashboard</h2>
-                <form class="needs-validation">
-                    <div class="col-sm-5">
-                        <label for="name">Name:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="name" :value="patient.name" readonly>
+          <div class="d-md-flex h-md-100">
+              <div class="col-md-6 h-md-100">
+                  <div class="d-md-flex h-100">
+                      <div class="">
+                        <h2 class="">Details</h2>
+                        <form>
+                          <div class="patient-info">
+                              <label for="name">Name:</label>
+                              <div class="input-group">
+                                  <input type="text" class="form-control" id="name" :value="patient.name" readonly>
+                              </div>
+                          </div>
+                          <div class="patient-info">
+                              <label for="username">Username:</label>
+                              <div class="input-group">
+                                  <input type="text" class="form-control" id="username" :value="patient.username" readonly>
+                              </div>
+                          </div>
+                          <div class="patient-info">
+                              <label for="email">Email:</label>
+                              <div class="input-group">
+                                  <input type="email" class="form-control" id="email" :value="patient.email" readonly>
+                              </div>
+                          </div>
+                          <div class="patient-info">
+                              <label for="healthNumber">Health Number:</label>
+                              <div class="input-group">
+                                  <input type="text" class="form-control" id="healthNumber" :value="patient.healthNumber" readonly>
+                              </div>
+                          </div>
+                          <div class="patient-info">
+                              <label for="associatedProfessional">Associated Professional:</label>
+                              <div class="input-group">
+                                  <input type="text" class="form-control" id="associatedProfessional" :value="patient.associatedProfessional" readonly>
+                              </div>
+                          </div>
+                        </form>
+                        <div>
+                          <nuxt-link :to="`/patients/${patient.username}/update`" class="btn btn-primary" >Update</nuxt-link>
                         </div>
-                    </div>
-                    <div class="col-sm-5">
-                        <label for="username">Username:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="username" :value="patient.username" readonly>
-                        </div>
-                    </div>
-                    <div class="col-sm-5">
-                        <label for="email">Email:</label>
-                        <div class="input-group">
-                            <input type="email" class="form-control" id="email" :value="patient.email" readonly>
-                        </div>
-                    </div>
-                    <div class="col-sm-5">
-                        <label for="healthNumber">Health Number:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="healthNumber" :value="patient.healthNumber" readonly>
-                        </div>
-                    </div>
-                    <div class="col-sm-5">
-                        <label for="associatedProfessional">Associated Professional:</label>
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="associatedProfessional" :value="patient.associatedProfessional" readonly>
-                        </div>
-                    </div>
-                </form>
+                      </div>
+                  </div>
+              </div>
 
-                <b-container>
-                  <nuxt-link :to="`/patients/${patient.username}/update`" class="btn btn-primary" >Update</nuxt-link>
-                </b-container>
-            </div>
-
+              <div class="col-md-6 h-md-100">
+                <div class="d-md-flex h-md-100">
+                  <h2 class="mb-3"> Biomedical Data</h2>
+                </div>
+                <div class="d-md-flex h-md-100">
+                  <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                    <b-table v-if="activePrescriptions.length > 0" striped over outlined :items="activePrescriptions" :fields="prescriptionFields" class="mt-3" style="width: 525px;"/>
+                    <p v-else>No patient data found.</p>
+                  </div>
+                </div>
+                <div>
+                  <nuxt-link :to="`/patients/${patient.username}/createData`" class="btn btn-primary" >Insert</nuxt-link>
+                </div>
+              </div>
+          </div>
             <div class="col-md-12 order-md-1">
                 <h2 class="mb-3"> Active Prescriptions</h2>
                 <b-table v-if="activePrescriptions.length > 0" striped over outlined :items="activePrescriptions" :fields="prescriptionFields" class="mt-3">
-
+                    <template v-slot:cell(actions)="row">
+                      <nuxt-link class="btn btn-primary btn-xs" :to="`/prescriptions/${row.item.id}/details`">
+                          <i class="fas fa-clipboard-list"></i>
+                      </nuxt-link>
+                    </template>
                 </b-table>
                 <p v-else>No active prescriptions.</p>
             </div>
@@ -53,11 +77,14 @@
             <div class="col-md-12 order-md-1">
                 <h2 class="mb-3"> Inactive Prescriptions</h2>
                 <b-table v-if="inactivePrescriptions.length > 0" striped over outlined :items="inactivePrescriptions" :fields="prescriptionFields" class="mt-3">
-
+                  <template v-slot:cell(actions)="row">
+                      <nuxt-link class="btn btn-primary btn-xs" :to="`/prescriptions/${row.item.id}/details`">
+                          <i class="fas fa-clipboard-list"></i>
+                      </nuxt-link>
+                    </template>
                 </b-table>
-                <p v-else>No active prescriptions.</p>
+                <p v-else>No inactive prescriptions.</p>
             </div>
-
             <b-container>
                 <nuxt-link to="/patients" class="btn btn-primary" >Back</nuxt-link>
             </b-container>
@@ -72,6 +99,7 @@
                 patient: {},
                 professional: {},
                 prescriptionFields: ['id', 'name', 'duration', 'actions' ],
+                patientDataFields: ['id', 'value', 'dataType', 'date']
             }
         },
         computed: {
@@ -83,6 +111,9 @@
             },
             inactivePrescriptions() {
                 return this.patient.inactivePrescriptionDTOs || []
+            },
+            patientData() {
+              return []
             }
         },
         created() {
@@ -98,5 +129,19 @@
 <style scoped>
   .page-body {
     padding-bottom: 50px;
+  }
+
+  .patient-info {
+    width: 300px;
+    margin-bottom: 10px;
+  }
+
+  .my-custom-scrollbar {
+    position: relative;
+    max-height: 390px;
+    overflow: auto;
+  }
+  .table-wrapper-scroll-y {
+    display: block;
   }
 </style>
