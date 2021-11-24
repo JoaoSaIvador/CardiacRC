af5ee700c56948cf68b948d61bd44c418e2666fe
alvestr <tomasalves2001@hotmail.com>
2021-11-24 12:34:34 +0000

    Prescription WS implemented

--- CardiacRC/src/main/java/dae/cardiacrc/ejbs/PrescriptionBean.java
+++ CardiacRC/src/main/java/dae/cardiacrc/ejbs/PrescriptionBean.java
@@ -9,6 +9,7 @@ import dae.cardiacrc.exceptions.MyEntityNotFoundException;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 import javax.validation.ConstraintViolationException;
 import java.util.List;
@@ -41,5 +42,67 @@ public class PrescriptionBean {
 
     public List<Prescription> getAllPrescriptions(){
         return (List<Prescription>) em.createNamedQuery("getAllPrescriptions").getResultList();
+    }
+
+    public Prescription findPrescription(int id) throws MyEntityNotFoundException {
+        Prescription prescription = em.find(Prescription.class, id);
+        if (prescription == null){
+            throw new MyEntityNotFoundException("Prescription not found!");
+        }
+        return prescription;
+    }
+
+    public void updatePrescription(int id, String professionalUsername, String patientUsername, String description, String name, int duration) throws MyEntityNotFoundException {
+        Prescription prescription = findPrescription(id);
+        Professional professional = em.find(Professional.class, professionalUsername);
+        if(professional == null) {
+            throw new MyEntityNotFoundException("Professional not found!");
+        }
+        Patient patient = em.find(Patient.class, patientUsername);
+        if(patient == null) {
+            throw new MyEntityNotFoundException("Patient not found!");
+        }
+
+        if (!prescription.getProfessional().equals(professional)){
+            prescription.getProfessional().removePrescription(prescription);
+            professional.addPrescription(prescription);
+        }
+        if (!prescription.getPatient().equals(patient)){
+            prescription.getPatient().removePrescription(prescription);
+            patient.addPrescription(prescription);
+        }
+
+        em.lock(prescription, LockModeType.OPTIMISTIC);
+        prescription.setProfessional(professional);
+        prescription.setPatient(patient);
+        prescription.setName(name);
+        prescription.setDescription(description);
+        prescription.setDuration(duration);
+
+        em.merge(prescription);
+    }
+
+    public void deletePresciption(int id) throws MyEntityNotFoundException {
+        Prescription prescription = findPrescription(id);
+        Professional professional = em.find(Professional.class, prescription.getProfessional());
+        if(professional == null) {
+            throw new MyEntityNotFoundException("Professional not found!");
+        }
+        Patient patient = em.find(Patient.class, prescription.getPatient());
+        if(patient == null) {
+            throw new MyEntityNotFoundException("Patient not found!");
+        }
+
+        patient.removePrescription(prescription);
+        professional.removePrescription(prescription);
+        em.remove(prescription);
+    }
+
+    public void toggleState(int id) throws MyEntityNotFoundException {
+        Prescription prescription = findPrescription(id);
+
+        em.lock(prescription, LockModeType.OPTIMISTIC);
+        prescription.setState(!prescription.isState());
+        em.merge(prescription);
     }
 }
